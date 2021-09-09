5786107d81983a0fa980c05ea47667f020cff168
rajat <rajat241994@gmail.com>
2021-09-09 20:22:16 +0530

    Add BASE_CIFAR100 experiment

--- utils/experiment.py
+++ utils/experiment.py
@@ -3,13 +3,17 @@ import json
 
 EXPERIMENTS_FILE = './experiments.json'
 BASE_EXPERIMENT = 'BASE_MNIST'
+FIELD_PARENT_EXPERIMENT = 'parent_experiment'
 
 def load_experiment_args(name: str) -> Dict:
     all_experiments = None
     with open(EXPERIMENTS_FILE, 'r') as f:
         all_experiments = json.load(f)
 
-    experiment = all_experiments[BASE_EXPERIMENT]
+    if all_experiments[name].get(FIELD_PARENT_EXPERIMENT) is not None:
+        experiment = load_experiment_args(all_experiments[name].get(FIELD_PARENT_EXPERIMENT))
+    else:
+        experiment = all_experiments[BASE_EXPERIMENT]
     if name != BASE_EXPERIMENT:
         merge_experiment(experiment, all_experiments[name])
 
